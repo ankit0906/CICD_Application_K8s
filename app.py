2cd0e13c9a139988fb47aba937a8e1459f0ef060
ankit0906 <kothawadeankit90@gmail.com>
2024-03-11 14:51:25 +0530

    changes

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def hello_world():
 <html>
 <body>
 <center>
-<h1>Demo on GitOps with ArgoCD and Github Actions.</h1> <br>
+<h1>Demo on GitOps with ArgoCD and Github Actions....</h1> <br>
 <br>
 <img src="https://github.com/ankit0906/CICD_Application_K8s/blob/main/itsworking.jpeg?raw=true">
 </center>
